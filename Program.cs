8330e0740914a8e14a575e97cf6e50acebcfeb75
Konstantin <k2rom@mail.ru>
2024-02-01 06:56:03 +0300

    Результат

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+int AccermanFunction(int n, int m)
+{
+    while (n != 0)
+    {
+        if (m == 0)
+        {
+            m = 1;
+        }
+        else
+        {
+            m = AccermanFunction(n, m - 1);
+        }
+        n--;
+    }
+    return m + 1;
+}
+
+
+Console.Clear();
+Console.Write("Введите неотрицательное число m ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите неотрицательное число n ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int result = AccermanFunction(n, m);
+
+Console.WriteLine($"Функция Аккермана ({n},{m}) равна {result}");
+
